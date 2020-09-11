4e7a6619af9bfc7980a328558708f76dfbfa52ed
Kenny Dong <54919493+dongKenny@users.noreply.github.com>
2020-09-11 03:58:32 -0700

    Add files via upload

--- artveeScraper.py
+++ artveeScraper.py
@@ -53,16 +53,30 @@ def createJSON(csvPath, jsonPath):
     with open(jsonPath, 'w', encoding='utf-8') as jsonW: 
         jsonW.write(json.dumps(data, indent=4)) 
 
-def getMeta(url, category):
+def getMeta(url, category, dataPath, writer, s3):
+
+    """
+    Parameters  
+    ----------
+    url : str
+        URL for the paginated category pages
+    category : str
+        String representation of the category used in the url
+    writer
+        CSV Writer passed in to be able to write the appended elements in data to the csv
+    s3 : botocore.client.S3
+        s3 client where the image can be uploaded
+    """
+
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     cards = soup.find_all("div", {"class" : re.compile("product-grid-item product woodmart-hover-tiled*")})
     imgSource = soup.find_all("a", {"class" : "product-image-link linko"})
 
-    #imgIndex refers to the image on the page, tracks which one is being downloaded of the 48
-    imgIndex = 0
+    #imgIndex refers to the image of the current card of the 48
+    #card refers to the div containing the image and its title/artist
 
-    #A card refers to the div containing the image and its title/artist
+    imgIndex = 0
     for card in cards:
         data = []
 
@@ -84,7 +98,11 @@ def getMeta(url, category):
             artistInfo = "Unknown"
             data.append(artistInfo)
 
-        #Get the download page from a card, use the soup parser to find the download link for the image
+        
+        # Find the download page in the card using the href
+        # Parse with soup to find the href which contains the download link to the image itself
+        # Write the image locally, upload to s3 bucket, remove the image
+
         imgdlPage = requests.get(imgSource[imgIndex].get("href"))
         imgSoup = BeautifulSoup(imgdlPage.content, "html.parser")
         imgLink = imgSoup.find("a", {"class" : "prem-link gr btn btn-secondary dis snax-action snax-action-add-to-collection snax-action-add-to-collection-downloads"}).get("href")
@@ -93,57 +111,71 @@ def getMeta(url, category):
 
         with open(imgPath, "wb") as imgFile:
             imgFile.write(requests.get(imgLink).content)
-            #Upload image to artvee bucket
+
             with open(imgPath, "rb") as s3Img:
                 s3.upload_fileobj(s3Img, "artvee", title + ".jpg")
             s3Img.close()
+
             imgFile.close()
 
-        #Remove locally downloaded image after uploading it and closing streams
+
         os.remove(imgPath)
         data.append(category)
         writer.writerow(data)
         imgIndex += 1
 
-s3 = boto3.client('s3')
-dataPath = ""               #INSERT THE PATH FOR THE CSV/JSON/TEMPORARY IMAGES
-csvPath = os.path.join(dataPath, "artvee.csv")
+def pageCounter(category):
 
-with open(csvPath, "w", newline = "", encoding="utf-8") as f:
+    """
+    Parse first page of a category
+    Find number of results displayed on page
+    mod 48 and add 1 for any remainder to get the total page numbers to iterate
+    """
 
-    if (dataPath == ""):
-        print("\nError: Please change the dataPath variable on line 109\n")
+    url = "https://artvee.com/c/%s/page/1/?per_page=48" % category
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("p", class_="woocommerce-result-count").text.strip("results").strip()
+    noPages = math.floor(int(results) / 48)
+
+    if (int(results) % 48 > 0):
+        noPages += 1
+    
+    return noPages
+
+def main():
+    s3 = boto3.client('s3')
+    create_bucket("artvee", "us-west-1")
+    dataPath = ""
+    csvPath = os.path.join(dataPath, "artvee.csv")
+            
+    with open(csvPath, "w", newline = "", encoding="utf-8") as f:
+
+        if (dataPath == ""):
+            print("\nError: Please assign a value to the dataPath \n")
+            f.close()
+
+        headers = ["Title", "Artist", "Category"]
+        writer = csv.writer(f)
+        writer.writerow(headers)
+
+        categories = ["abstract", "figurative", "landscape", "religion", "mythology", "posters", "animals", "illustration", "fashion", "still-life", "historical", "botanical", "drawings", "japanese-art"]
+
+        for category in categories:
+            noPages = pageCounter(category)
+
+            #Pagination
+            for p in range(1, noPages + 1):
+                print("Currently looking at: %s, page %d" % (category, p))
+                url = "https://artvee.com/c/%s/page/%d/?per_page=48" % (category, p)
+                getMeta(url, category, dataPath, writer, s3)
+
         f.close()
 
-    writer = csv.writer(f)
-    headers = ["Title", "Artist", "Category"]
-    writer.writerow(headers)
+    jsonPath = dataPath + "/artvee.json"
+    createJSON(csvPath, jsonPath)
+    
+    with open(jsonPath, "rb") as s3Meta:
+        s3.upload_fileobj(s3Meta, "artvee", "artveeMeta.json")
 
-    categories = ["abstract", "figurative", "landscape", "religion", "mythology", "posters", "animals", "illustration", "fashion", "still-life", "historical", "botanical", "drawings", "japanese-art"]
-
-    for category in categories:
-        #Parse first page of a category, find number of results, mod 48 and add 1 for any remainder to get the total page numbers to iterate
-        url = "https://artvee.com/c/%s/page/1/?per_page=48" % category
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        results = soup.find("p", class_="woocommerce-result-count").text.strip("results").strip()
-        noPages = math.floor(int(results) / 48)
-
-        if (int(results) % 48 > 0):
-            noPages += 1
-
-        for p in range(1, noPages + 1):
-            print("Currently looking at: %s, page %d" % (category, p))
-            url = "https://artvee.com/c/%s/page/%d/?per_page=48" % (category, p)
-            getMeta(url, category)
-
-    f.close()
-
-jsonPath = dataPath + "/artvee.json"
-createJSON(csvPath, jsonPath)
-
-#Create s3 bucket, upload json
-create_bucket("artvee", "us-west-1")
-#response = s3.list_buckets()
-with open(jsonPath, "rb") as s3Meta:
-    s3.upload_fileobj(s3Meta, "artvee", "artveeMeta.json")
+main()
